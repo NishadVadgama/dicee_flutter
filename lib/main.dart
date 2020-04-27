844f645bfbbe7aabf4297f4744c35442621032c7
Nishad Vadgama <nishad.vadgama@gmail.com>
2020-04-27 13:05:04 -0400

    Both dice face randomly change on click of any one dice

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceValue = 1;
+  int rightDiceValue = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    changeDice() {
+      setState(() {
+        leftDiceValue = Random().nextInt(6) + 1;
+        rightDiceValue = Random().nextInt(6) + 1;
+      });
+    }
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () => changeDice(),
+              padding: EdgeInsets.all(16.0),
+              child: Image.asset('images/dice$leftDiceValue.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () => changeDice(),
+              padding: EdgeInsets.all(16.0),
+              child: Image.asset('images/dice$rightDiceValue.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
